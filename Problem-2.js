31504ce71ad1feeef228a3730602040325086cec
Mehedi Hasan Sagor <123135797+mehediHsagor@users.noreply.github.com>
2024-11-08 23:21:47 +0600

    Delete Problem-2.js

--- Problem-2.js
+++
@@ -1,6 +0,0 @@
-{
-    function removeDuplicates(numbers) {
-        return numbers.filter(function (x, y) { return numbers.indexOf(x) === y; });
-    }
-    console.log(removeDuplicates([1, 2, 2, 3, 3, 4, 4, 5]));
-}
