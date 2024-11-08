70f0db120da83e4888291333dd3215a1617e0be7
Mehedi Hasan Sagor <123135797+mehediHsagor@users.noreply.github.com>
2024-11-08 23:22:30 +0600

    Delete Problem-5.js

--- Problem-5.js
+++
@@ -1,8 +0,0 @@
-{
-    function getProperty(obj, key) {
-        return obj[key];
-    }
-    // Sample usage
-    var person = { name: "Alice", age: 30 };
-    console.log(getProperty(person, "name"));
-}
